3d99a9e0a727c0c86d3f96383014ce881335cf50
Pietro Gagliardi <pietro10@mac.com>
2012-12-31 12:38:52 -0500

    Cleaned up fuse.go.

--- fuse.go
+++ fuse.go
@@ -5,88 +5,112 @@ import (
 	"os"
 	"github.com/hanwen/go-fuse/fuse"
 	"path/filepath"
-"fmt"
+	"errors"
 )
+
+var errNoSuchGame = errors.New("no such game")
+var errGameNotFound = errors.New("game not found")
 
 // note to self: this needs to be an embed in a struct as DefaultFileSystem will implement the  methods I don't override here and have them return fuse.ENOSYS
 type mamefuse struct {
 	fuse.DefaultFileSystem
 }
 
+func getgame(gamename string) (*Game, error) {
+	g, ok := games[gamename]
+	if !ok {				// not a valid game
+		return nil, errNoSuchGame
+	}
+//	ret := make(chan string)
+//	zipRequests <- zipRequest{
+//		Game:	gamename,
+//		Return:	ret,
+//	}
+//	zipname := <-ret
+//	close(ret)
+//	if zipname == "" {		// none given
+	good, err := g.Find()
+	if err != nil {
+		return nil, err
+	}
+	if !good {
+		return nil, errGameNotFound
+	}
+	return g, nil
+}
+
+func getgame_fuseerr(gamename string) (*Game, fuse.Status) {
+	g, err := getgame(gamename)
+	if err == errNoSuchGame {
+		return nil, fuse.EINVAL
+	} else if err == errGameNotFound {
+		return nil, fuse.ENOENT
+	} else if err != nil {
+		// TODO report error
+		return nil, fuse.EIO
+	}
+	return g, fuse.OK
+}
+
+func getloopbackfile(filename string) (*fuse.LoopbackFile, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	// according to the go-fuse source (fuse/file.go), fuse.LoopbackFile will take ownership of our *os.FIle, calling Close() on it itself
+	return &fuse.LoopbackFile{
+		File:	f,
+	}, nil
+}
+
+func getloopbackfile_fuseerr(filename string) (*fuse.LoopbackFile, fuse.Status) {
+	loopfile, err := getloopbackfile(filename)
+	if err != nil {
+		// TODO report error
+		return nil, fuse.EIO
+	}
+	return loopfile, fuse.OK
+}
+
+func getattr(filename string) (*fuse.Attr, fuse.Status) {
+	stat, err := os.Stat(filename)
+	if err != nil {
+		// TODO report error
+		return nil, fuse.EIO
+	}
+	return fuse.ToAttr(stat), fuse.OK
+}
+
 func (fs *mamefuse) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
-fmt.Print("getattr " + name + " ")
 	basename := filepath.Base(name)
 	switch filepath.Ext(basename) {
 	case ".zip":				// ROM set
 		gamename := basename[:len(basename) - 4]
-fmt.Println("game:" + gamename, " ")
-		if _, ok := games[gamename]; !ok {		// not a valid game
-fmt.Println()
-			return nil, fuse.ENOENT
+		g, err := getgame_fuseerr(gamename)
+		if err != fuse.OK {
+			return nil, err
 		}
-//		ret := make(chan string)
-/*		zipRequests <- zipRequest{
-			Game:	gamename,
-			Return:	ret,
-		}
-*///		zipname := <-ret
-//		close(ret)
-//		if zipname == "" {		// none given
-		good, err := games[gamename].Find()
-		if err != nil || !good {
-			// TODO handle error
-			return nil, fuse.ENOENT
-		}
-//		f, err := os.Open(zipname)
-		f, err := os.Open(games[gamename].ROMLoc)
-		if err != nil {
-			// TODO report error
-			return nil, fuse.EIO	// TODO proper error
-		}
-		// according to the go-fuse source (fuse/file.go), fuse.LoopbackFile will take ownership of our *os.FIle, calling Close() on it itself
-		loopfile := &fuse.LoopbackFile{
-			File:	f,
-		}
-		var a fuse.Attr
-		status := loopfile.GetAttr(&a)
-		loopfile.Release()
-fmt.Println(a, " ", status)
-		return &a, status
+//		return getattr(g.ROMLoc)
+		// TODO merely returning getattr() always results in
+		// 2012/12/31 12:13:27 writer: Write/Writev failed, err: 22=invalid argument. opcode: LOOKUP
+		// but this works
+		a, err := getattr(g.ROMLoc)
+		return a, err
 	}
-fmt.Println()
 	return nil, fuse.ENOENT		// any other file is invalid
 }
 
 func (fs *mamefuse) Open(name string, flags uint32, context *fuse.Context) (file fuse.File, code fuse.Status) {
-fmt.Println(name)
 	basename := filepath.Base(name)
 	switch filepath.Ext(basename) {
 	case ".zip":				// ROM set
 		gamename := basename[:len(basename) - 4]
-//		ret := make(chan string)
-/*		zipRequests <- zipRequest{
-			Game:	gamename,
-			Return:	ret,
+		g, err := getgame_fuseerr(gamename)
+		if err != fuse.OK {
+			return nil, err
 		}
-*///		zipname := <-ret
-//		close(ret)
-//		if zipname == "" {		// none given
-		good, err := games[gamename].Find()
-		if err != nil || !good {
-			// TODO handle error
-			return nil, fuse.ENOENT
-		}
-//		f, err := os.Open(zipname)
-		f, err := os.Open(games[gamename].ROMLoc)
-		if err != nil {
-			// TODO report error
-			return nil, fuse.EIO	// TODO proper error
-		}
-		// according to the go-fuse source (fuse/file.go), fuse.LoopbackFile will take ownership of our *os.FIle, calling Close() on it itself
-		loopfile := &fuse.LoopbackFile{
-			File:	f,
-		}
-		return loopfile, fuse.OK
+		// TODO worry about closing the file?
+		return getloopbackfile_fuseerr(g.ROMLoc)
 	case ".chd":				// CHD
 		// ...
 	case "":					// folder
@@ -97,11 +121,6 @@ fmt.Println(name)
 }
 
 func (fs *mamefuse) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
-fmt.Println("opendir " + name)
-	if name == "" {
-		return []fuse.DirEntry{
-			{ Name: "test" },
-		}, fuse.OK
-	}
+	// TODO
 	return nil, fuse.ENOENT
 }
